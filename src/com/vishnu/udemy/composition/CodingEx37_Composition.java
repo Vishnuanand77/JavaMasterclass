c100ee462dd68fd9976a90a01a72864268605526
Vishnu Anand <vishnuanand2000@gmail.com>
2022-02-01 12:42:19 +0530

    Java Composition

--- src/com/vishnu/udemy/composition/CodingEx37_Composition.java
+++ src/com/vishnu/udemy/composition/CodingEx37_Composition.java
@@ -1,17 +1,23 @@
 package com.vishnu.udemy.composition;
 
 public class CodingEx37_Composition {
-    Wall wall1 = new Wall("West");
-    Wall wall2 = new Wall("East");
-    Wall wall3 = new Wall("South");
-    Wall wall4 = new Wall("North");
 
-    Ceiling ceiling = new Ceiling(12, 55);
+    public static void main(String[] args) {
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
 
-    Bed bed = new Bed("Modern", 4, 3,2, 1);
+        Ceiling ceiling = new Ceiling(12, 55);
 
-    Lamp lamp = new Lamp("Classic", false, 75);
+        Bed bed = new Bed("Modern", 4, 3,2, 1);
 
-    Bedroom bedRoom = new Bedroom("Vishnu Anand", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedRoom = new Bedroom("Vishnu Anand", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+        bedRoom.makeBed();
+
+        bedRoom.getLamp().turnOn();
+    }
 
 }
